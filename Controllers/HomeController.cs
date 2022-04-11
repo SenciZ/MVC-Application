d5e2734f26ab2934dfd3ab0e2b762ed6c1443474
Senad Zenkovic <zenkovic88@gmail.com>
2022-04-11 18:39:11 -0500

    create new view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace MVC_Application.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
