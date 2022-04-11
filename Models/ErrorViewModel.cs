e5ec4b3046c6c072473ffce13ce24d0154d0dad2
Senad Zenkovic <zenkovic88@gmail.com>
2022-04-11 18:15:36 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MVC_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
